8302fc23d5fcd366c188ef5c056cb29210d119e8
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-10-04 19:44:27 -0500

    links routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{height: '100vh', overflow: 'auto', position: 'relative'}}>
+        <Layout style={{background: 'url(https://bit.ly/2P9Vm0I) center / cover'}}>
+            <Header transparent title="Title" style={{color: 'white'}}>
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </Navigation>
+            </Drawer>
+              <div className="page-content">
+                <Main />
+              </div>
+            <Content>
+              
+            </Content>
+        </Layout>
       </div>
     );
   }
