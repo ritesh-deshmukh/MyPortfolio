3c24ef25b070f1cc1d95b0f989e2e00e91d1103b
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-10-11 09:03:03 -0500

    contact update

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,8 +9,8 @@ class Contact extends Component {
 						<Cell col={6}>
 							<h2>Ritesh Deshmukh</h2>
 							<img src="https://bit.ly/2NpGz0q" alt="avatar" style={{height: "250px"}} />
-							<p style={{width: "75%", margin: "auto", paddingTop: "1em"}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-							Explicabo labore, neque praesentium provident iste blanditiis saepe repudiandae necessitatibus nisi assumenda deleniti, dolorum error repellat.</p>
+							<p style={{width: "75%", margin: "auto", paddingTop: "1em"}}>Front End Enthusiast. Back End devotee. 
+																									Love working on a Full Stack</p>
 						</Cell>
 						<Cell col={6}>
 							<h2>Contact Me</h2>
@@ -20,37 +20,40 @@ class Contact extends Component {
 									<ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
 									    	<i className="fa fa-phone-square" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    		+1 (469) 954-4171
 									    </ListItemContent>
 									  </ListItem>
 									  <ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
 									    	<i className="fa fa-home" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    		127.0.0.1
 									    </ListItemContent>
 									  </ListItem>
 									  <ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
 									    	<i className="fa fa-envelope" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    		riteshsunildeshmukh@gmail.com
 									    </ListItemContent>
 									</ListItem>
 									 <ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
-									    	<i className="fa fa-linkedin" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    	<i className="fa fa-linkedin" aria-hidden="true" />{' '}
+									    		<a href="https://www.linkedin.com/in/ritesh-deshmukh/"
+									    			target = "_blank" rel="noopener noreferrer">ritesh-deshmukh</a>
 									    </ListItemContent>
 									</ListItem>
 									 <ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
 									    	<i className="fa fa-twitter" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    		<a href="https://twitter.com/ritesh_deshmukh"
+									    			target = "_blank" rel="noopener noreferrer">ritesh_deshmukh</a>
 									    </ListItemContent>
 									</ListItem>
 									 <ListItem>
 									    <ListItemContent style={{fontSize: "25px", fontFamily: "Anton"}}>
-									    	<i className="fa fa-facebook" aria-hidden="true" />
-									    		(xxx)xxx-xxxx
+									    	<i className="fa fa-facebook" aria-hidden="true" />&nbsp;&nbsp;&nbsp;&nbsp;
+									    		<a href="https://www.facebook.com/riteshd.93"
+									    			target = "_blank" rel="noopener noreferrer">riteshd.93</a>
 									    </ListItemContent>
 									</ListItem>
 								</List>	
